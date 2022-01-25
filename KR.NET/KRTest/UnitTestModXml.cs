92288525c31551b0ba4d147f8d871ded5b5bab03
Livio <lfrancis@alice.it>
2022-01-25 19:32:28 +0100

    Conversione di progetto KR Livio : XML - Traduzione del modulo da VB6 - Creazione unit test del modulo

---
+++ KR.NET/KRTest/UnitTestModXml.cs
@@ -0,0 +1,34 @@
+﻿using KR.NET;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace KRTest
+{
+    [TestClass]
+    public class UnitTestModXml
+    {
+        [TestMethod]
+        public void TestMethodConvXML()
+        {
+            string input = "<prova>";
+            string outConvXML = MOD_XML.ConvToXML(input);
+            string outConvXmlExpected = "&lt;prova&gt;";
+            Assert.AreEqual(outConvXmlExpected, outConvXML);
+
+            input = "'provag'";
+            outConvXML = MOD_XML.ConvToXML(input);
+            outConvXmlExpected = input;
+            Assert.AreEqual(outConvXmlExpected, outConvXML);
+
+            input = "prova prova&prova";
+            outConvXML = MOD_XML.ConvToXML(input);
+            outConvXmlExpected = input.Replace("&", "&amp;").Replace(" ", "&#32;");
+            Assert.AreEqual(outConvXmlExpected, outConvXML);
+
+            input = "prova\r\n";
+            outConvXML = MOD_XML.ConvToXML(input);
+            outConvXmlExpected = input.Replace("\r", "&#13;").Replace("\n", "&#10;");
+            Assert.AreEqual(outConvXmlExpected, outConvXML);
+        }
+    }
+}
---
+++ KR.NET/MOD_XML.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KR.NET
+{
+    public class MOD_XML
+    {
+        public static string ConvToXML(string strS , int intForce = 0)
+        {
+            string strS2 = ""; int i; char Chr;
+            bool bBlank = true;
+            for (i = 0; i < strS.Length; i++)
+            {
+                Chr = strS[i];
+                int intChr = (int)Chr;
+                if (intChr <= 31)
+                {
+                    strS2 += "&#" + intChr.ToString() + ";";
+                    if (intChr != 9 && intChr != 13 && intChr != 10) bBlank = false;
+                } else if (intChr >= 128)
+                {
+                    strS2 += "&#" + intChr.ToString() + ";";
+                } else
+                {
+                    switch (intChr)
+                    {
+                        case 32: 
+                            strS2 += "&#32;";
+                            break;
+                        case 38:
+                            strS2 += "&amp;";
+                            bBlank = false;
+                            break;
+                        case 39:
+                            strS2 += "'";
+                            bBlank = false;
+                            break;
+                        case 60:
+                            strS2 += "&lt;";
+                            bBlank = false;
+                            break;
+                        case 62:
+                            strS2 += "&gt;";
+                            bBlank = false;
+                            break;
+                        default:
+                            strS2 += Chr;
+                            bBlank = false;
+                            break;
+                    }
+                }
+            }
+            if (bBlank && intForce == 0) strS2 = strS;
+            return strS2;
+        }
+    }
+}

fd7d885595484a3a0ca644a702810ffb03f29140
Livio <lfrancis@alice.it>
2022-01-26 16:10:11 +0100

    Conversione di progetto KR Livio : Form KR - Creazione di tutti i controlli

--- KR.NET/kr.Designer.cs
+++ KR.NET/kr.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // kr
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1207, 649);
+            this.Name = "kr";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion

62195472ca8240ff03d458bbf1f4c513cdefa82d
Livio <lfrancis@alice.it>
2022-01-31 11:41:26 +0100

    Conversione di progetto KR Livio : miniKR con gestione

---
+++ KR.NET/minKR.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace KR.NET
+{
+    partial class minKR
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtChiave = new System.Windows.Forms.TextBox();
+            this.btnVai = new System.Windows.Forms.Button();
+            this.btnEsci = new System.Windows.Forms.Button();
+            this.lblStato = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtChiave
+            // 
+            this.txtChiave.Location = new System.Drawing.Point(12, 11);
+            this.txtChiave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtChiave.Name = "txtChiave";
+            this.txtChiave.PasswordChar = '*';
+            this.txtChiave.Size = new System.Drawing.Size(693, 22);
+            this.txtChiave.TabIndex = 5;
+            this.txtChiave.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtChiave_KeyPress);
+            // 
+            // btnVai
+            // 
+            this.btnVai.Location = new System.Drawing.Point(711, 10);
+            this.btnVai.Name = "btnVai";
+            this.btnVai.Size = new System.Drawing.Size(75, 23);
+            this.btnVai.TabIndex = 6;
+            this.btnVai.Text = "VAI";
+            this.btnVai.UseVisualStyleBackColor = true;
+            this.btnVai.Click += new System.EventHandler(this.btnVai_Click);
+            // 
+            // btnEsci
+            // 
+            this.btnEsci.Location = new System.Drawing.Point(792, 10);
+            this.btnEsci.Name = "btnEsci";
+            this.btnEsci.Size = new System.Drawing.Size(75, 23);
+            this.btnEsci.TabIndex = 7;
+            this.btnEsci.Text = "Esci";
+            this.btnEsci.UseVisualStyleBackColor = true;
+            this.btnEsci.Click += new System.EventHandler(this.btnEsci_Click);
+            // 
+            // lblStato
+            // 
+            this.lblStato.AutoEllipsis = true;
+            this.lblStato.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblStato.Location = new System.Drawing.Point(12, 8);
+            this.lblStato.Name = "lblStato";
+            this.lblStato.Size = new System.Drawing.Size(858, 25);
+            this.lblStato.TabIndex = 10;
+            this.lblStato.Text = "Stato: PRONTO";
+            // 
+            // minKR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 40);
+            this.Controls.Add(this.btnEsci);
+            this.Controls.Add(this.btnVai);
+            this.Controls.Add(this.txtChiave);
+            this.Controls.Add(this.lblStato);
+            this.Name = "minKR";
+            this.Text = "minKR";
+            this.Activated += new System.EventHandler(this.minKR_Activated);
+            this.Load += new System.EventHandler(this.minKR_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox txtChiave;
+        private System.Windows.Forms.Button btnVai;
+        private System.Windows.Forms.Button btnEsci;
+        private System.Windows.Forms.Label lblStato;
+    }
+}
---
+++ KR.NET/minKR.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using Application = System.Windows.Forms.Application;
+using MessageBox = System.Windows.MessageBox;
+using MessageBoxButtons = System.Windows.MessageBoxButton;
+
+namespace KR.NET
+{
+    public partial class minKR : Form
+    {
+        public minKR()
+        {
+            InitializeComponent();
+        }
+
+        private void ManageControls(bool boolInvert)
+        {
+            lblStato.Visible = boolInvert;
+            btnVai.Visible = !boolInvert; txtChiave.Visible = !boolInvert;
+            btnEsci.Visible = !boolInvert;
+        }
+
+        private void btnEsci_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void minKR_Load(object sender, EventArgs e)
+        {
+            ManageControls(false);
+            if (! "".Equals(MOD_MAIN.G_strChiave))
+                txtChiave.Text = MOD_MAIN.G_strChiave;
+        }
+
+        private void btnVai_Click(object sender, EventArgs e)
+        {
+            EseguiKLog();
+        }
+
+        private void minKR_Activated(object sender, EventArgs e)
+        {
+            if (!"".Equals(txtChiave.Text))
+                Esegui();
+        }
+
+        private void txtChiave_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string strS = "";
+            if (e.KeyChar == ((char)13))
+                Esegui();
+        }
+
+        private void Esegui()
+        {
+            string strS = txtChiave.Text;
+            ///strS = MOD_KLOG.CaricaLogFile(strS, MOD_MAIN.G_strFileLog); Non so come fa a funzionare nell'originale ma lo correggo
+            string strS1 = MOD_KLOG.CaricaLogFile(strS, MOD_MAIN.G_strFileLog);
+            if ("".Equals(strS1))
+            {
+                MessageBox.Show("Chiave inserita non corretta", "Esegui crypt dei file", MessageBoxButtons.OK, MessageBoxImage.Exclamation);
+                this.Close();
+            }
+            else
+            {
+                MOD_KLOG.RigeneraLog(strS, MOD_MAIN.G_strDirRoot, MOD_MAIN.G_strFileLog, MOD_MAIN.G_strDirRoot);
+                EseguiKLog();
+            }
+        }
+
+        private void EseguiKLog()
+        {
+            string strNomeFile = "", strS = "", strIn = "";
+            string strErr = ""; long lngCount = 0, i = 0;
+            ManageControls(true);
+            //2. CaricaLogFile
+            strS = txtChiave.Text;
+            strS = MOD_KLOG.CaricaLogFile(strS, MOD_MAIN.G_strFileLog);
+            if ("".Equals(strS))
+            {
+                MessageBox.Show("Chiave inserita non valida", "Esegui crypt dei file", MessageBoxButtons.OK, MessageBoxImage.Exclamation);
+                this.Close();
+            }
+            if (MOD_MAIN.G_bolErrLog)
+            {
+                MessageBox.Show("ORMATO LOG NON VALIDO", "Esegui crypt dei file", MessageBoxButtons.OK, MessageBoxImage.Exclamation);
+                this.Close();
+            }
+            //3. Generazione della lista di tutti i file
+            lblStato.Text = "Stato: Generazione della lista dei file da criptare....";
+            lblStato.Refresh();
+            strErr = MOD_FILE_LIST.Genera(1, MOD_MAIN.G_strDirRoot, MOD_MAIN.G_strFileLog);
+            if (!"".Equals(strErr))
+            {
+                MessageBox.Show(strErr, "Esegui crypt dei file", MessageBoxButtons.OK, MessageBoxImage.Exclamation);
+                this.Close();
+            }
+            //Criptatura di ogni file della lista
+            lngCount = MOD_FILE_LIST.GetSize();
+            if (lngCount <=0)
+            {
+                MessageBox.Show(strErr, "Non sono stati trovati files all'interno della directory", MessageBoxButtons.OK, MessageBoxImage.Exclamation);
+                this.Close();
+            }
+            for (i = 0; i < lngCount; i++)
+            {
+                //3.1 Crpt del file(che cambia anche nome)
+                strNomeFile = MOD_FILE_LIST.GetFile(i);
+                //3.2 Conversione chiave da testo ad array di byte
+                if (i % 1000 == 0) Application.DoEvents();
+                lblStato.Text = "Stato: kritp di " + i.ToString() + "/" + lngCount.ToString();
+                lblStato.Refresh();
+                MOD_PRG_UTILS.Kriptp(strNomeFile, txtChiave.Text);
+            }
+            //4. SalvaLogFile
+            MOD_KLOG.SalvaLogFile(txtChiave.Text, MOD_MAIN.G_strFileLog);
+            //5. Operazione Completata
+            MessageBox.Show("Operazione completata", "Esegui crypt dei file", MessageBoxButtons.OK, MessageBoxImage.Information);
+            lblStato.Text = "Stato: PRONTO!";
+            lblStato.Refresh();
+            if (!"".Equals(MOD_MAIN.G_strErr))
+            {
+                MOD_MAIN.G_strErr = "<?xml version=\"1.01\" encoding=\"UTF-8\"?><EXCEPTIONS DATETIME=\"" + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString() + "\">" + MOD_MAIN.G_strErr + "</EXCEPTIONS>";
+                MOD_UTILS_SO.ErrorLog(MOD_MAIN.G_strErr);
+            }
+            Application.Exit();
+        }
+    }
+}

6592699b726712a2f2829147a450ed3984579e21
Livio <lfrancis@alice.it>
2022-02-01 12:13:06 +0100

    Conversione di progetto KR Livio : Progetto KRDE

---
+++ KRDE/frmDecrypt.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace KRDE
+{
+    partial class frmDecrypt
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Text1 = new System.Windows.Forms.TextBox();
+            this.Text2 = new System.Windows.Forms.TextBox();
+            this.Text3 = new System.Windows.Forms.TextBox();
+            this.btnEsci = new System.Windows.Forms.Button();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File klog";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Chiave1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Chiave2";
+            // 
+            // Text1
+            // 
+            this.Text1.Location = new System.Drawing.Point(119, 6);
+            this.Text1.Name = "Text1";
+            this.Text1.Size = new System.Drawing.Size(777, 22);
+            this.Text1.TabIndex = 3;
+            // 
+            // Text2
+            // 
+            this.Text2.Location = new System.Drawing.Point(119, 37);
+            this.Text2.Name = "Text2";
+            this.Text2.Size = new System.Drawing.Size(776, 22);
+            this.Text2.TabIndex = 4;
+            // 
+            // Text3
+            // 
+            this.Text3.Location = new System.Drawing.Point(119, 72);
+            this.Text3.Name = "Text3";
+            this.Text3.Size = new System.Drawing.Size(776, 22);
+            this.Text3.TabIndex = 5;
+            // 
+            // btnEsci
+            // 
+            this.btnEsci.Location = new System.Drawing.Point(802, 100);
+            this.btnEsci.Name = "btnEsci";
+            this.btnEsci.Size = new System.Drawing.Size(94, 23);
+            this.btnEsci.TabIndex = 8;
+            this.btnEsci.Text = "Esci";
+            this.btnEsci.UseVisualStyleBackColor = true;
+            this.btnEsci.Click += new System.EventHandler(this.btnEsci_Click);
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(15, 100);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(103, 23);
+            this.btnGet.TabIndex = 9;
+            this.btnGet.Text = "Chiave";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // frmDecrypt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 132);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.btnEsci);
+            this.Controls.Add(this.Text3);
+            this.Controls.Add(this.Text2);
+            this.Controls.Add(this.Text1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDecrypt";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Text1;
+        private System.Windows.Forms.TextBox Text2;
+        private System.Windows.Forms.TextBox Text3;
+        private System.Windows.Forms.Button btnEsci;
+        private System.Windows.Forms.Button btnGet;
+    }
+}
+
---
+++ KRDE/frmDecrypt.cs
@@ -0,0 +1,32 @@
+﻿using KRLib.NET;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KRDE
+{
+    public partial class frmDecrypt : Form
+    {
+        public frmDecrypt()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEsci_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            Text2.Text = MOD_INVKEY.reverseKey(Text1.Text, true);
+            Text3.Text = MOD_INVKEY.reverseKey(Text1.Text, false);
+        }
+    }
+}

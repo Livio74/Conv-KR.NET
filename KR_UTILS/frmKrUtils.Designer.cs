277a5ba9ddb4e6d5a320691fbfe9f15eebefac38
Livio <lfrancis@alice.it>
2022-02-01 15:12:31 +0100

    Conversione di progetto KR Livio : Progetto KR_UTILS

---
+++ KR_UTILS/frmKrUtils.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace KR_UTILS
+{
+    partial class frmKrUtils
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtPWD = new System.Windows.Forms.TextBox();
+            this.btnEsci = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Directory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PWD";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(99, 8);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(852, 22);
+            this.txtPath.TabIndex = 2;
+            // 
+            // txtPWD
+            // 
+            this.txtPWD.Location = new System.Drawing.Point(99, 42);
+            this.txtPWD.Name = "txtPWD";
+            this.txtPWD.Size = new System.Drawing.Size(395, 22);
+            this.txtPWD.TabIndex = 3;
+            // 
+            // btnEsci
+            // 
+            this.btnEsci.Location = new System.Drawing.Point(857, 41);
+            this.btnEsci.Name = "btnEsci";
+            this.btnEsci.Size = new System.Drawing.Size(94, 23);
+            this.btnEsci.TabIndex = 9;
+            this.btnEsci.Text = "Esci";
+            this.btnEsci.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(500, 41);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(94, 23);
+            this.btnOK.TabIndex = 10;
+            this.btnOK.Text = "&OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // frmKrUtils
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 77);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnEsci);
+            this.Controls.Add(this.txtPWD);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmKrUtils";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtPWD;
+        private System.Windows.Forms.Button btnEsci;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
+
---
+++ KR_UTILS/frmKrUtils.cs
@@ -0,0 +1,50 @@
+﻿using KRLib.NET;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
+using MessageBoxButtons = System.Windows.MessageBoxButton;
+
+namespace KR_UTILS
+{
+    public partial class frmKrUtils : Form
+    {
+        public frmKrUtils()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            string strLog; DateTime dateM; string strS;
+            strLog = txtPath.Text;
+            if (strLog[strLog.Length - 1] != '\\') strLog += '\\';
+            strLog += "klog.txt";
+            dateM = File.GetLastWriteTime(strLog);
+            if (MOD_UTILS_SO.ExistsFile(strLog))
+            {
+                Encoding iso88591 = Encoding.GetEncoding("ISO-8859-1");
+                StreamReader streamFileLog = new StreamReader(strLog, iso88591, false);
+                strS = streamFileLog.ReadLine();
+                strS = STATICUTILS.EventuallyRemoveDoubleQuotes(strS);
+                streamFileLog.Close();
+                int first = dateM.Year;
+                int second = dateM.Day * dateM.Month;
+                string strS1 = dateM.ToString("yyyyMMdd") + first.ToString("X") + dateM.ToString("ddyyyyMM") + second.ToString("X");
+                txtPWD.Text = MOD_INVKEY.InvKript(strS1, strS, false);
+            }
+            else
+            {
+                MessageBox.Show("file log KR non trovato", "Kr Utils", MessageBoxButtons.OK, MessageBoxImage.Exclamation);
+            }
+        }
+    }
+}
